8e77323d39d1d88b0f2c05d1900468238325c64e
corfeur12 <corfeur12@gmail.com>
2020-03-18 12:14:09 +0000

    fixed locate, removed terminator as latest ppa is for xenial

--- ubuntu-setup-script.sh
+++ ubuntu-setup-script.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-user=$USER
+user=$(logname)
 tmp_dir="/tmp/auto-install/"
 install="apt install -y"
 update="apt update -y"
@@ -17,30 +17,32 @@ $update
 apt upgrade -y
 
 if ! [ -e /usr/lib/snapd ] ; then
-	$install snapd -y
+	$install snapd
 fi
 
-$install locate moreutils htop curl wget jq unzip git gnome-tweaks xclip
+$install mlocate moreutils htop curl wget jq unzip git gnome-tweaks xclip
 
 $install ubuntu-restricted-extras
 
 $install firefox
 
-add-apt-repository ppa:gnome-terminator
-$update
-$install terminator
+#add-apt-repository -y ppa:gnome-terminator/xenial
+#$update
+#$install terminator
 
 $install python3-dev python3-pip build-essential libssl-dev libffi-dev python3-setuptools
 
 $install gedit
 
-sudo gpasswd -a $USER input
+gpasswd -a $user input
+libinput_gestures_dir="/home/${user}/.libinput-gestures"
 $install xdotool wmctrl libinput-tools
-git clone https://github.com/bulletmark/libinput-gestures.git
-cd libinput-gestures
+git clone https://github.com/bulletmark/libinput-gestures.git "${libinput_gestures_dir}"
+cd "${libinput_gestures_dir}"
 sudo make install
 libinput-gestures-setup autostart
 libinput-gestures-setup start
+cd "${tmp_dir}"
 
 $install zsh
 chsh -s $(which zsh)
